29a7d8b149b4769c20a0d498b4838cf5c6bad615
WB <wasilliy.b@gmail.com>
2022-08-21 20:38:00 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <pre>{{res}}</pre>
+    <pre>{{getResult}}</pre>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+<script>
+
+import api from '@/api.js'
+export default {
+  name: 'App',
+    data(){
+      return {
+        res:[],
+        res2:[],
+        getResult: []
+      }
+    },
+    async mounted() {
+      try {
+        const res = await api.get("/albums/1/photos");
+        this.res=res
+      } catch (err) {
+        this.getResult = this.fortmatResponse(err.response?.data) || err;
+      }
+
+      api.get("/albums/1/photos").then((response) => {
+        this.res2 = response.data
+      }).catch((error) => {
+        console.log('Show error notification!')
+        return Promise.reject(error)
+      })
+    }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
